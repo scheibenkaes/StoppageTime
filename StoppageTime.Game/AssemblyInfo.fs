70adc6024a6381e93652b3e2181034d0553f7556
Benjamin Klüglein <scheibenkaes@googlemail.com>
2015-05-16 22:51:25 +0200

    init

---
+++ StoppageTime.Game/AssemblyInfo.fs
@@ -0,0 +1,18 @@
+﻿module StoppageTime.Game.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly:AssemblyTitle("StoppageTime.Game")>]
+[<assembly:AssemblyDescription("")>]
+[<assembly:AssemblyConfiguration("")>]
+[<assembly:AssemblyCompany("")>]
+[<assembly:AssemblyProduct("")>]
+[<assembly:AssemblyCopyright("Benjamin Klüglein")>]
+[<assembly:AssemblyTrademark("")>]
+[<assembly:AssemblyVersion("1.0.0.0")>]
+()
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
